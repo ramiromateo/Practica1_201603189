6b719321e9612171c6b784b20de9558059da1c6a
P <P@P-PC>
2017-08-24 19:55:02 -0600

    Final del practica1

---
+++ src/Practica1_201603189/Practica1_201603189.java
@@ -0,0 +1,9 @@
+package Practica1_201603189;
+
+public class Practica1_201603189 {
+
+    public static void main(String[] args) {
+              Buscamina_201603189 obj=new Buscamina_201603189();
+    }
+    
+}
